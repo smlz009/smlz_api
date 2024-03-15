fd0575b95f8485dd859bb424e78e0d41cc480d63
smlz009 <1259826159@qq.com>
2024-03-15 12:02:24 +0800

    feat(koa-static): ✨ koa-static

--- src/app/index.js
+++ src/app/index.js
@@ -3,12 +3,16 @@ const bodyParser = require('koa-bodyparser')
 const cors = require('@koa/cors')
 const path = require('path')
 const registerRouter = require('../router')
+const serve = require('koa-static')
+
+// 设置静态资源文件夹
+const staticPath = '../static'
 
 //创建app
 const app = new koa()
 //使用中间件
 app.use(bodyParser())
-app.use(cors())
+app.use(serve(path.join(__dirname, staticPath)))
 
 //注册路由
 registerRouter(app)
